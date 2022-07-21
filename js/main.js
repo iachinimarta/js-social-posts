7af4d8a1c144f17f165b417a4bda1b17990f67a7
Marta Iachini <iachinimarta@gmail.com>
2022-07-21 10:03:56 +0200

    Modifiche: - cambio colore a "mi piace" dopo il click

--- js/main.js
+++ js/main.js
@@ -60,13 +60,12 @@ const containerDom = document.getElementById('container');
 
 posts.forEach(post => {
 
-
     containerDom.innerHTML += 
     `<div class="post">
         <div class="post__header">
             <div class="post-meta">                    
                 <div class="post-meta__icon">
-                    <img class="profile-pic" src=${post.author.image} alt="${"?"}">                    
+                    <img class="profile-pic" src=${post.author.image} alt="${post.author.name}">                    
                 </div>
                 <div class="post-meta__data">
                     <div class="post-meta__author">${post.author.name}</div>
@@ -92,5 +91,17 @@ posts.forEach(post => {
             </div> 
         </div>            
     </div>`;
+
 });
 
+let likeBtn = document.getElementsByClassName('js-like-button');
+
+for (let i = 0; i < likeBtn.length; i++) {
+    likeBtn[i].addEventListener('click',
+        function(event) {
+            event.preventDefault();
+
+            likeBtn[i].classList.add('like-button--liked');
+        }
+    );
+}
