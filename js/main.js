e6eb07ddfbf2bb7aa5b06fed70ecc73da8050a56
Marta Iachini <iachinimarta@gmail.com>
2022-07-21 09:41:53 +0200

    Modifiche: - rimosse alcune parti di prova e aggiunte in un file a parte

--- js/main.js
+++ js/main.js
@@ -56,42 +56,9 @@ const posts = [
     }
 ];
 
-
-const dataOggi = new Date('2022 07 20');
-const meseOggi = dataOggi.getMonth() + 1;
-const annoOggi = dataOggi.getFullYear();
-
 const containerDom = document.getElementById('container');
 
 posts.forEach(post => {
-
-    let dataIeri = new Date(post.created);
-    let meseIeri = dataIeri.getMonth() + 1;
-    let annoIeri = dataIeri.getFullYear();
-
-    let differenzaMesi = meseOggi - meseIeri;
-
-    let differenzaAnni = annoOggi - annoIeri;
-    
-    let meseDinamico = "mese";
-
-    if (differenzaMesi < 0) {
-        differenzaMesi = differenzaMesi * -1;
-        meseDinamico = "mesi";
-    } else if (differenzaMesi > 1) {
-        meseDinamico = "mesi";
-    }
-
-    let qualcosa = post.author;
-
-    const {name, image} = qualcosa;
-
-    if (image == null) {
-        customAlt = "?";
-    } else {
-        customAlt = name;
-    }
-
 
     containerDom.innerHTML += 
     `<div class="post">
---
+++ js/prove.js
@@ -0,0 +1,30 @@
+const dataOggi = new Date('2022 07 20');
+const meseOggi = dataOggi.getMonth() + 1;
+const annoOggi = dataOggi.getFullYear();
+
+let dataIeri = new Date(post.created);
+let meseIeri = dataIeri.getMonth() + 1;
+let annoIeri = dataIeri.getFullYear();
+
+let differenzaMesi = meseOggi - meseIeri;
+
+let differenzaAnni = annoOggi - annoIeri;
+
+let meseDinamico = "mese";
+
+if (differenzaMesi < 0) {
+    differenzaMesi = differenzaMesi * -1;
+    meseDinamico = "mesi";
+} else if (differenzaMesi > 1) {
+    meseDinamico = "mesi";
+}
+
+let qualcosa = post.author;
+
+const {name, image} = qualcosa;
+
+if (image == null) {
+    customAlt = "?";
+} else {
+    customAlt = name;
+}
